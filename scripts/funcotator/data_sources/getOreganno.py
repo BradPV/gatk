2c5e3ff21a58c8c3fb5f6d6587cec7034c6d76ee
Jonn Smith <jonn@broadinstitute.org>
2017-12-19 16:09:13 -0500

    Fixed more bugs in the oreganno script.

--- scripts/funcotator/data_sources/getOreganno.py
+++ scripts/funcotator/data_sources/getOreganno.py
@@ -25,7 +25,7 @@ OREGANNO_KEYS = ['Outcome', 'Type', 'Gene_Symbol', 'Gene_ID', 'Gene_Source', 'Re
 VALUES_DELIMITER = '|'
 NON_VALUE = "N/A"
 
-ENABLE_STDOUT = False
+ENABLE_STDOUT = True
 
 ########################################################################
 # Functions:
@@ -68,49 +68,54 @@ def get_file_from_url(url, file_name):
 
     f.close()
 
+
+def row_comparator(x,y):
+
+    chrCmp = cmp(x['Chr'], y['Chr'])
+    if chrCmp == 0:
+        startCmp = cmp(int(x['Start']), int(y['Start']))
+        if startCmp == 0:
+            return cmp(int(x['End']), int(y['End']))
+        else:
+            return startCmp
+    else:
+        return chrCmp
+
+
 ########################################################################
 # Main:
 
 if __name__ == '__main__':
 
-    print 'Downloading Oreganno file:', RAW_FILE_NAME, '...',
-    FLUSH()
+    if not ENABLE_STDOUT: print 'Processing OReganno files ...'
+
+    if ENABLE_STDOUT: print 'Downloading Oreganno file:', RAW_FILE_NAME, '...',
+    if ENABLE_STDOUT: FLUSH()
     # Download the Raw file:
-    get_file_from_url(FILE_URL, RAW_FILE_NAME)
-    print 'DONE!'
-    FLUSH()
+    # get_file_from_url(FILE_URL, RAW_FILE_NAME)
+    if ENABLE_STDOUT: print 'DONE!'
+    if ENABLE_STDOUT: FLUSH()
 
     # Get the version of the file:
     file_version = RAW_FILE_NAME.replace('ORegAnno_Combined_', '').replace('.tsv', '').replace('.', '')
 
-    print 'Creating output files ...',
-    FLUSH()
-    # Set up the output files:
-    out_hg19_tsv_writer = csv.DictWriter(file(OUT_HG19_FILE_NAME_TEMPLATE % file_version, 'w'), OUTPUT_HEADERS,
-                                         delimiter='\t', lineterminator="\n")
-    out_hg19_tsv_writer.fieldnames = OUTPUT_HEADERS
-    out_hg19_tsv_writer.writeheader()
-    out_hg38_tsv_writer = csv.DictWriter(file(OUT_HG38_FILE_NAME_TEMPLATE % file_version, 'w'), OUTPUT_HEADERS,
-                                         delimiter='\t', lineterminator="\n")
-    out_hg38_tsv_writer.fieldnames = OUTPUT_HEADERS
-    out_hg38_tsv_writer.writeheader()
-    print 'DONE!'
-    FLUSH()
-
     # Now that we have the file, go through it and reformat it:
     tsvReader = GenericTsvReader(RAW_FILE_NAME)
     headers = tsvReader.getFieldNames()
-    print('Found headers (input): ' + str(headers))
-    FLUSH()
+    if ENABLE_STDOUT: print('Found headers (input): ' + str(headers))
+    if ENABLE_STDOUT: FLUSH()
 
-    print 'Iterating through input file ...',
-    FLUSH()
+    hg19_dataset = []
+    hg38_dataset = []
+
+    if ENABLE_STDOUT: print 'Iterating through input file ...',
+    if ENABLE_STDOUT: FLUSH()
     # Go through our file:
     for lineNum, line in enumerate(tsvReader):
 
         if line['Species'].lower() != 'homo sapiens':
-            print '    Ignoring non-human record on line:', lineNum, " : ", line['Species'].lower()
-            FLUSH()
+            if ENABLE_STDOUT: print '    Ignoring non-human record on line:', lineNum, " : ", line['Species'].lower()
+            if ENABLE_STDOUT: FLUSH()
             continue
 
         # Get the trivial fields here:
@@ -125,13 +130,52 @@ if __name__ == '__main__':
         row['Values'] = get_values_data_from_row_dict(line)
 
         if line['Build'].lower() == 'hg19':
-            out_hg19_tsv_writer.writerow(row)
+            hg19_dataset.append(row)
         elif line['Build'].lower() == 'hg38':
-            out_hg38_tsv_writer.writerow(row)
+            hg38_dataset.append(row)
         else:
-            print '    Ignoring incompatible build record on line:', lineNum, " : ", line['Build'].lower()
-            FLUSH()
+            if ENABLE_STDOUT: print '    Ignoring incompatible build record on line:', lineNum, " : ", line['Build'].lower()
+            if ENABLE_STDOUT: FLUSH()
+
+    if ENABLE_STDOUT: print 'DONE!'
+    if ENABLE_STDOUT: FLUSH()
+
+    if ENABLE_STDOUT: print 'Creating output files ...',
+    if ENABLE_STDOUT: FLUSH()
+    # Set up the output files:
+    out_hg19_tsv_writer = csv.DictWriter(file(OUT_HG19_FILE_NAME_TEMPLATE % file_version, 'w'), OUTPUT_HEADERS,
+                                         delimiter='\t', lineterminator="\n")
+    out_hg19_tsv_writer.fieldnames = OUTPUT_HEADERS
+    out_hg19_tsv_writer.writeheader()
+    out_hg38_tsv_writer = csv.DictWriter(file(OUT_HG38_FILE_NAME_TEMPLATE % file_version, 'w'), OUTPUT_HEADERS,
+                                         delimiter='\t', lineterminator="\n")
+    out_hg38_tsv_writer.fieldnames = OUTPUT_HEADERS
+    out_hg38_tsv_writer.writeheader()
+    if ENABLE_STDOUT: print 'DONE!'
+    if ENABLE_STDOUT: FLUSH()
+    
+    print 'Sorting hg19 dataset ...',
+    FLUSH()
+    hg19_dataset.sort(row_comparator)
     print 'DONE!'
     FLUSH()
 
+    print 'Writing hg19 dataset ...',
+    FLUSH()
+    for row in hg19_dataset:
+        out_hg19_tsv_writer.writerow(row)
+    print 'DONE!'
+    FLUSH()
 
+    print 'Sorting hg38 dataset ...',
+    FLUSH()
+    hg38_dataset.sort(row_comparator)
+    print 'DONE!'
+    FLUSH()
+
+    print 'Writing hg38 dataset ...',
+    FLUSH()
+    for row in hg38_dataset:
+        out_hg38_tsv_writer.writerow(row)
+    print 'DONE!'
+    FLUSH()
